9ad45ff40c376cc78f55d2245d611bbd2ac81379
iblea <iblea@iasdf.com>
2024-03-30 11:40:22 +0000

    docker 시 webAllowOthers 옵션 주석 추가

--- studyspring/sql/h2.sh
+++ studyspring/sql/h2.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # jdbc:h2:tcp://localhost/~/test
+# add -webAllowOthers option in docker
 dir=$(dirname "$0")
 java -cp "$dir/h2-2.2.224.jar:$H2DRIVERS:$CLASSPATH" org.h2.tools.Console -webAllowOthers "$@"
