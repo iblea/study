c3322f91c1f736ecafb5133242970374fd60b3de
iblea <iblea@iasdf.com>
2024-12-07 18:14:39 +0900

    programmers cpp template 추가

---
+++ programmers/template/solution.cpp
@@ -0,0 +1,54 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+using namespace std;
+
+string solution() {
+    return "template_function";
+}
+
+
+
+#define CLASS_NAME gtest_template
+
+class CLASS_NAME: public ::testing::Test {
+private:
+	clock_t t0;
+	clock_t t1;
+protected:
+	CLASS_NAME() { }
+	~CLASS_NAME() override { }
+
+	void SetUp() override { set_start_time(); }
+	void TearDown() override { set_end_time(); get_duration_time(); }
+
+	void set_start_time() {
+		this->t0 = clock(); // or gettimeofday or whatever
+	}
+	void set_end_time() {
+		this->t1 = clock(); // or gettimeofday or whatever
+	}
+
+	// 시간 관련 체크
+	void get_duration_time() {
+		double elapsedSec = (double)(this->t1 - this->t0) / (double)1000000;
+		double elapsedMS = (double)(this->t1 - this->t0) / (double)1000;
+
+		cout << setprecision(3) << fixed << endl << "Duration of Times : ";
+		cout << elapsedMS << "ms, " << elapsedSec << "sec" << endl << endl;
+	}
+
+};
+
+TEST_F(CLASS_NAME, __LINE__)
+{
+	EXPECT_EQ(solution(), "template_function");
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
