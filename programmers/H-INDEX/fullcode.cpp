6a442f34a77c76a395964e473ae30d2f5e5172de
iblea <iblea@iasdf.com>
2024-12-08 02:49:57 +0900

    프로그래머스 - H-Index

---
+++ programmers/H-INDEX/fullcode.cpp
@@ -0,0 +1,88 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+using namespace std;
+
+/*
+H-Index는 과학자의 생산성과 영향력을 나타내는 지표입니다. 어느 과학자의 H-Index를 나타내는 값인 h를 구하려고 합니다. 위키백과1에 따르면, H-Index는 다음과 같이 구합니다.
+어떤 과학자가 발표한 논문 n편 중, h번 이상 인용된 논문이 h편 이상이고 나머지 논문이 h번 이하 인용되었다면 h의 최댓값이 이 과학자의 H-Index입니다.
+어떤 과학자가 발표한 논문의 인용 횟수를 담은 배열 citations가 매개변수로 주어질 때, 이 과학자의 H-Index를 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+과학자가 발표한 논문의 수는 1편 이상 1,000편 이하입니다.
+논문별 인용 횟수는 0회 이상 10,000회 이하입니다.
+
+입출력 예 설명
+[3, 0, 6, 1, 5]	3
+이 과학자가 발표한 논문의 수는 5편이고, 그중 3편의 논문은 3회 이상 인용되었습니다. 그리고 나머지 2편의 논문은 3회 이하 인용되었기 때문에 이 과학자의 H-Index는 3입니다.
+*/
+
+#include <algorithm>
+int solution(vector<int> citations) {
+    int h_index = 0;
+
+    sort(citations.begin(), citations.end(), greater<int>());
+
+    for (h_index = citations.size(); h_index > 0; h_index--) {
+        int count_high = 0;
+        int count_low = 0;
+
+        for (int citation : citations) {
+            if (citation >= h_index) {
+                count_high++;
+            }
+            if (citation <= h_index) {
+                count_low++;
+            }
+        }
+        if (count_high >= h_index && count_low <= h_index) {
+            break;
+        }
+    }
+    return h_index;
+}
+
+
+#define CLASS_NAME gtest_template
+
+class CLASS_NAME: public ::testing::Test {
+private:
+	clock_t t0;
+	clock_t t1;
+protected:
+	CLASS_NAME() { }
+	~CLASS_NAME() override { }
+
+	void SetUp() override { set_start_time(); }
+	void TearDown() override { set_end_time(); get_duration_time(); }
+
+	void set_start_time() {
+		this->t0 = clock(); // or gettimeofday or whatever
+	}
+	void set_end_time() {
+		this->t1 = clock(); // or gettimeofday or whatever
+	}
+
+	// 시간 관련 체크
+	void get_duration_time() {
+		double elapsedSec = (double)(this->t1 - this->t0) / (double)1000000;
+		double elapsedMS = (double)(this->t1 - this->t0) / (double)1000;
+
+		cout << setprecision(3) << fixed << endl << "Duration of Times : ";
+		cout << elapsedMS << "ms, " << elapsedSec << "sec" << endl << endl;
+	}
+
+};
+
+TEST_F(CLASS_NAME, __LINE__)
+{
+	EXPECT_EQ(solution({ 3, 0, 6, 1, 5 }), 3);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
